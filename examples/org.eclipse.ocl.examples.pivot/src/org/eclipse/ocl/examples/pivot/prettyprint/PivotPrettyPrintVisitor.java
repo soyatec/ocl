eca3d5063a0fa32150fb707d9aebce25b287957f
Ed.Willink <ed@willink.me.uk>
2012-05-27 15:18:31 +0100

    [unrelated] Lose duplicate constants

--- examples/org.eclipse.ocl.examples.pivot/src/org/eclipse/ocl/examples/pivot/prettyprint/PivotPrettyPrintVisitor.java
+++ examples/org.eclipse.ocl.examples.pivot/src/org/eclipse/ocl/examples/pivot/prettyprint/PivotPrettyPrintVisitor.java
@@ -16,7 +16,6 @@
  */
 package org.eclipse.ocl.examples.pivot.prettyprint;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.ocl.examples.pivot.Class;
@@ -35,66 +34,11 @@ import org.eclipse.ocl.examples.pivot.util.AbstractExtendingVisitor;
 import org.eclipse.ocl.examples.pivot.util.Visitable;
 
 /**
- * The PrettyPrintNameVisitor supports pretty printing of a type-like signature.
+ * The PivotPrettyPrintVisitor supports pretty printing of a Pivot model elements.
  * PrettyPrintOptions may be used to configure the printing.
  */
 public class PivotPrettyPrintVisitor extends AbstractExtendingVisitor<Object,PrettyPrinter>
 {
-	public static final String NULL_PLACEHOLDER = "<null>";
-	public static List<String> reservedNameList = Arrays.asList("and", "else", "endif", "false", "if", "implies", "in", "invalid", "let", "not", "null", "or", "self", "then", "true", "xor");
-	public static List<String> restrictedNameList = Arrays.asList("Bag", "Boolean", "Collection", "Integer", "OclAny", "OclInvalid", "OclVoid", "OrderedSet", "Real", "Sequence", "Set", "String", "Tuple", "UnlimitedNatural");
-	
-/*	public static Global createOptions(Namespace scope) {
-		PrettyPrintOptions.Global options = new PrettyPrintOptions.Global(scope);
-		options.addReservedNames(PivotPrettyPrintVisitor.reservedNameList);
-		options.addRestrictedNames(PivotPrettyPrintVisitor.reservedNameList);
-		options.setUseParentheses(true);
-		return options;
-	}
-
-	public static String prettyPrint(Visitable element) {
-		PrettyPrintOptions.Global options = createOptions(null);
-		PrettyPrintContext context = new PrettyPrintContext(options);
-		PivotPrettyPrintVisitor visitor = new PivotPrettyPrintVisitor(context);
-		try {
-			visitor.safeVisit(element);
-			return visitor.toString();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return visitor.toString() + " ... " + e.getClass().getName() + " - " + e.getLocalizedMessage();
-		}
-	}
-
-	public static String prettyPrint(Visitable element, Namespace namespace) {
-		PrettyPrintOptions.Global options = createOptions(namespace);
-		PrettyPrintContext context = new PrettyPrintContext(options);
-		PivotPrettyPrintVisitor visitor = new PivotPrettyPrintVisitor(context);
-		try {
-			context.safeVisit(element);
-			return visitor.toString();
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return visitor.toString() + " ... " + e.getClass().getName() + " - " + e.getLocalizedMessage();
-		}
-	}
-	
-	public static String prettyPrint(Visitable element, PrettyPrintOptions options) {
-		PrettyPrintContext context = new PrettyPrintContext(options);
-		PivotPrettyPrintVisitor visitor = new PivotPrettyPrintVisitor(context);
-		try {
-			visitor.safeVisit(element);
-			String string = visitor.toString();
-//			System.out.println("Type-prettyPrint : " + element.eClass().getName() + "/" + element.eClass().getName() + " => " + string);
-			return string;
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			return visitor.toString() + " ... " + e.getClass().getName() + " - " + e.getLocalizedMessage();
-		}
-	} */
-	
 	public PivotPrettyPrintVisitor(PrettyPrinter context) {
 		super(context);
 	}
@@ -279,6 +223,6 @@ public class PivotPrettyPrintVisitor extends AbstractExtendingVisitor<Object,Pre
 	}
 
 	public String visiting(Visitable visitable) {
-		return (visitable == null)? NULL_PLACEHOLDER : visitable.getClass().getName();
+		return (visitable == null)? PrettyPrinter.NULL_PLACEHOLDER : visitable.getClass().getName();
 	}
 }
