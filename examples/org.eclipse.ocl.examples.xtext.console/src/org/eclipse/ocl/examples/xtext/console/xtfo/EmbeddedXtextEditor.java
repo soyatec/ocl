41ebbd568be3b025fbc99d7f4aa2c0856e1a5063
Ed Willink <ed@willink.me.uk>
2013-05-20 15:59:30 +0100

    [releng] Update Pivot copyrights

--- examples/org.eclipse.ocl.examples.xtext.console/src/org/eclipse/ocl/examples/xtext/console/xtfo/EmbeddedXtextEditor.java
+++ examples/org.eclipse.ocl.examples.xtext.console/src/org/eclipse/ocl/examples/xtext/console/xtfo/EmbeddedXtextEditor.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2010,2011 ProxiAD and Others
+ * Copyright (c) 2010, 2012 ProxiAD and Others
  * 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
