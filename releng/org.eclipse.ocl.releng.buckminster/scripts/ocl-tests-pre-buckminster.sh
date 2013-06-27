42746472cf38e1ba19737749d469f2dd2a8b2d64
Adolfo SBH <adolfosbh@gmail.com>
2013-06-27 14:30:54 +0100

    [410792] - [releng] Update Hudson jobs for Luna - Changes for the maintenance branch counterpath

--- releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-tests-pre-buckminster.sh
+++ releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-tests-pre-buckminster.sh
@@ -55,7 +55,7 @@ COMPACT_BUILD_ID="${COMPACT_BUILD_ID:0:12}"
 	echo "packages.base=MDT-OCL.downloads"
 	echo "tests.base=MDT-OCL.test.results"
 	echo "component.name=tools"
-	echo "version=4.1.0"
+	echo "version=4.1.1"
 	echo "downloads.area=/home/data/httpd/download.eclipse.org/modeling/mdt/ocl"
 
 
