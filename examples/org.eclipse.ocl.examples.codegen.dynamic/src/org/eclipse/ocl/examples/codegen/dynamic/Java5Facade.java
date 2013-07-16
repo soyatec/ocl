b5597a1ae9db1798144a255db3aa973435a66c3a
Ed Willink <ed@willink.me.uk>
2013-07-16 15:59:59 +0100

    [unrelated] Avoid deep reference problem for Java 5 accessors

---
+++ examples/org.eclipse.ocl.examples.codegen.dynamic/src/org/eclipse/ocl/examples/codegen/dynamic/Java5Facade.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2013 E.D.Willink and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     E.D.Willink - initial API and implementation
+ *
+ * </copyright>
+ */
+package org.eclipse.ocl.examples.codegen.dynamic;
+
+public class Java5Facade
+{
+	public static Class<?> loadClass(String qualifiedName, String javaCodeSource) throws Exception {
+		return OCL2JavaFileObject.loadClass(qualifiedName, javaCodeSource);
+	}
+}
