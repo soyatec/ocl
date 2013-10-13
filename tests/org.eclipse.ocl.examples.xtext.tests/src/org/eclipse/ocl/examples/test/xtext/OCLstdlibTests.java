c9028ed058f16e56eb1732d73b988479023955e7
Ed.Willink <ed@willink.me.uk>
2013-10-13 14:29:31 +0100

    [418412] Test that Pivot.oclas is loadable

--- tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/test/xtext/OCLstdlibTests.java
+++ tests/org.eclipse.ocl.examples.xtext.tests/src/org/eclipse/ocl/examples/test/xtext/OCLstdlibTests.java
@@ -29,6 +29,8 @@ import org.eclipse.emf.common.EMFPlugin;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.ocl.examples.domain.elements.DomainOperation;
 import org.eclipse.ocl.examples.domain.library.LibraryFeature;
@@ -40,6 +42,7 @@ import org.eclipse.ocl.examples.pivot.Constraint;
 import org.eclipse.ocl.examples.pivot.Element;
 import org.eclipse.ocl.examples.pivot.ExpressionInOCL;
 import org.eclipse.ocl.examples.pivot.Feature;
+import org.eclipse.ocl.examples.pivot.Library;
 import org.eclipse.ocl.examples.pivot.TemplateParameter;
 import org.eclipse.ocl.examples.pivot.Type;
 import org.eclipse.ocl.examples.pivot.TypedElement;
@@ -47,7 +50,9 @@ import org.eclipse.ocl.examples.pivot.context.ModelContext;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManager;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceAdapter;
 import org.eclipse.ocl.examples.pivot.manager.MetaModelManagerResourceSetAdapter;
+import org.eclipse.ocl.examples.pivot.model.OCLMetaModel;
 import org.eclipse.ocl.examples.pivot.model.OCLstdlib;
+import org.eclipse.ocl.examples.pivot.resource.ASResourceFactoryRegistry;
 import org.eclipse.ocl.examples.pivot.utilities.AS2Moniker;
 import org.eclipse.ocl.examples.xtext.base.utilities.BaseCSResource;
 import org.eclipse.ocl.examples.xtext.base.utilities.CS2PivotResourceAdapter;
@@ -96,6 +101,91 @@ public class OCLstdlibTests extends XtextTestCase
 			}
 		}
 		return map;
+	}
+
+	protected void doLoadAS(@NonNull ResourceSet resourceSet, @NonNull URI libraryURI, @NonNull Resource javaResource, boolean validateMonikers) {
+		Resource asResource = resourceSet.getResource(libraryURI, true);
+		assertNoResourceErrors("Load failed", asResource);
+		assertNoUnresolvedProxies("File Model", asResource);
+		assertNoValidationErrors("File Model", asResource);
+//		PivotAliasCreator.refreshPackageAliases(javaResource);
+		assertNoResourceErrors("Java Model", javaResource);
+		assertNoUnresolvedProxies("Java Model", javaResource);
+		assertNoValidationErrors("Java Model", javaResource);
+		if (!validateMonikers) {
+			return;
+		}
+		//
+		//	Check similar content
+		//
+		Map<String,Element> fileMoniker2PivotMap = computeMoniker2PivotMap(Collections.singletonList(asResource));
+//		for (String moniker : fileMoniker2PivotMap.keySet()) {
+//			System.out.println("File : " + moniker);
+//		}
+		Map<String,Element> javaMoniker2PivotMap = computeMoniker2PivotMap(Collections.singletonList(javaResource));
+//		for (String moniker : javaMoniker2PivotMap.keySet()) {
+//			System.out.println("Java : " + moniker);
+//		}
+//		assertEquals(fileMoniker2PivotMap.size(), javaMoniker2PivotMap.size());
+		for (String moniker : fileMoniker2PivotMap.keySet()) {
+			Element fileElement = fileMoniker2PivotMap.get(moniker);
+			Element javaElement = javaMoniker2PivotMap.get(moniker);
+			if (javaElement == null) {
+				boolean isExpression = false;
+				for (EObject eObject = fileElement; eObject != null; eObject = eObject.eContainer()) {
+					if ((eObject instanceof ExpressionInOCL) || (eObject instanceof Constraint) || (eObject instanceof Annotation)) {
+						isExpression = true;		// Embedded OCL not present in Java
+						break;
+					}
+				}
+				if (isExpression) {
+					continue;
+				}
+			}
+			assertNotNull("Missing java element for '" + moniker + "'", javaElement);
+//			@SuppressWarnings("null")	// Can be null and we'll have an NPE as the test failure.
+			Class<? extends Element> javaElementClass = javaElement.getClass();
+			assertEquals(fileElement.getClass(), javaElementClass);
+			if (fileElement instanceof TypedElement) {
+				Type fileType = ((TypedElement)fileElement).getType();
+				Type javaType = ((TypedElement)javaElement).getType();
+				assertEquals(fileType.getClass(), javaType.getClass());
+				String fileMoniker = AS2Moniker.toString(fileType);
+				String javaMoniker = AS2Moniker.toString(javaType);
+				assertEquals(fileMoniker, javaMoniker);
+			}
+			if (fileElement instanceof Feature) {
+				String fileClass = ((Feature)fileElement).getImplementationClass();
+				String javaClass = ((Feature)javaElement).getImplementationClass();
+				if (fileClass == null) {
+					LibraryFeature implementation = ((Feature)fileElement).getImplementation();
+					if (implementation != null) {
+						fileClass = implementation.getClass().getCanonicalName();
+					}
+				}
+				if (javaClass == null) {
+					LibraryFeature implementation = ((Feature)javaElement).getImplementation();
+					if (implementation != null) {
+						javaClass = implementation.getClass().getCanonicalName();
+					}
+				}
+				assertEquals(fileClass, javaClass);
+			}
+			if (fileElement instanceof Type) {
+				List<Element> fileTypes = new ArrayList<Element>(((Type)fileElement).getSuperClass());
+				List<Element> javaTypes = new ArrayList<Element>(((Type)javaElement).getSuperClass());
+				Collections.sort(fileTypes, MonikeredComparator.INSTANCE);
+				Collections.sort(javaTypes, MonikeredComparator.INSTANCE);
+				assertEquals(fileTypes.size(), javaTypes.size());
+				for (int i = 0; i < fileTypes.size(); i++) {
+					Element fileType = fileTypes.get(i);
+					Element javaType = javaTypes.get(i);
+					String fileMoniker = AS2Moniker.toString(fileType);
+					String javaMoniker = AS2Moniker.toString(javaType);
+					assertEquals(fileMoniker, javaMoniker);
+				}
+			}
+		}
 	}
 
 	protected Resource doLoadFromString(@NonNull String fileName, @NonNull String testFile) throws Exception {
@@ -282,92 +372,36 @@ public class OCLstdlibTests extends XtextTestCase
 		//
 		//	Load OCL stdlib as an AS file.
 		//
+		ResourceSet resourceSet = new ResourceSetImpl();
 		if (!EMFPlugin.IS_ECLIPSE_RUNNING) {			
 			new ProjectMap().initializeResourceSet(resourceSet);
 		}
 		URI libraryURI = URI.createPlatformResourceURI("org.eclipse.ocl.examples.library/model-gen/OCL-2.5.oclas", true);
-		Resource asResource = resourceSet.getResource(libraryURI, true);
-		assertNoResourceErrors("Load failed", asResource);
-		assertNoUnresolvedProxies("File Model", asResource);
-		assertNoValidationErrors("File Model", asResource);
 		//
 		//	Load 'oclstdlib.oclstdlib' as pre-code-generated Java.
 		//
 		Resource javaResource = OCLstdlib.getDefault();
-//		PivotAliasCreator.refreshPackageAliases(javaResource);
-		assertNoResourceErrors("Java Model", javaResource);
-		assertNoUnresolvedProxies("Java Model", javaResource);
-		assertNoValidationErrors("Java Model", javaResource);
+		doLoadAS(resourceSet, libraryURI, javaResource, true);
+	}
+	
+	/**
+	 * Checks that the OCL 2.5 AS model is the same as the pre-compiled
+	 * Java implementation.
+	 */
+	public void testPivot_AS() throws Exception {
 		//
-		//	Check similar content
+		//	Load OCL stdlib as an AS file.
 		//
-		Map<String,Element> fileMoniker2PivotMap = computeMoniker2PivotMap(Collections.singletonList(asResource));
-//		for (String moniker : fileMoniker2PivotMap.keySet()) {
-//			System.out.println("File : " + moniker);
-//		}
-		Map<String,Element> javaMoniker2PivotMap = computeMoniker2PivotMap(Collections.singletonList(javaResource));
-//		for (String moniker : javaMoniker2PivotMap.keySet()) {
-//			System.out.println("Java : " + moniker);
-//		}
-//		assertEquals(fileMoniker2PivotMap.size(), javaMoniker2PivotMap.size());
-		for (String moniker : fileMoniker2PivotMap.keySet()) {
-			Element fileElement = fileMoniker2PivotMap.get(moniker);
-			Element javaElement = javaMoniker2PivotMap.get(moniker);
-			if (javaElement == null) {
-				boolean isExpression = false;
-				for (EObject eObject = fileElement; eObject != null; eObject = eObject.eContainer()) {
-					if ((eObject instanceof ExpressionInOCL) || (eObject instanceof Constraint) || (eObject instanceof Annotation)) {
-						isExpression = true;		// Embedded OCL not present in Java
-						break;
-					}
-				}
-				if (isExpression) {
-					continue;
-				}
-			}
-			assertNotNull("Missing java element for '" + moniker + "'", javaElement);
-//			@SuppressWarnings("null")	// Can be null and we'll have an NPE as the test failure.
-			Class<? extends Element> javaElementClass = javaElement.getClass();
-			assertEquals(fileElement.getClass(), javaElementClass);
-			if (fileElement instanceof TypedElement) {
-				Type fileType = ((TypedElement)fileElement).getType();
-				Type javaType = ((TypedElement)javaElement).getType();
-				assertEquals(fileType.getClass(), javaType.getClass());
-				String fileMoniker = AS2Moniker.toString(fileType);
-				String javaMoniker = AS2Moniker.toString(javaType);
-				assertEquals(fileMoniker, javaMoniker);
-			}
-			if (fileElement instanceof Feature) {
-				String fileClass = ((Feature)fileElement).getImplementationClass();
-				String javaClass = ((Feature)javaElement).getImplementationClass();
-				if (fileClass == null) {
-					LibraryFeature implementation = ((Feature)fileElement).getImplementation();
-					if (implementation != null) {
-						fileClass = implementation.getClass().getCanonicalName();
-					}
-				}
-				if (javaClass == null) {
-					LibraryFeature implementation = ((Feature)javaElement).getImplementation();
-					if (implementation != null) {
-						javaClass = implementation.getClass().getCanonicalName();
-					}
-				}
-				assertEquals(fileClass, javaClass);
-			}
-			if (fileElement instanceof Type) {
-				List<Element> fileTypes = new ArrayList<Element>(((Type)fileElement).getSuperClass());
-				List<Element> javaTypes = new ArrayList<Element>(((Type)javaElement).getSuperClass());
-				Collections.sort(fileTypes, MonikeredComparator.INSTANCE);
-				Collections.sort(javaTypes, MonikeredComparator.INSTANCE);
-				assertEquals(fileTypes.size(), javaTypes.size());
-				for (int i = 0; i < fileTypes.size(); i++) {
-					Element fileType = fileTypes.get(i);
-					Element javaType = javaTypes.get(i);
-					String fileMoniker = AS2Moniker.toString(fileType);
-					String javaMoniker = AS2Moniker.toString(javaType);
-					assertEquals(fileMoniker, javaMoniker);
-				}
-			}
-		}
+		ResourceSet resourceSet = new ResourceSetImpl();
+		new ProjectMap().initializeResourceSet(resourceSet);
+		ASResourceFactoryRegistry.INSTANCE.configureResourceSet(resourceSet);
+		URI pivotURI = URI.createPlatformResourceURI("org.eclipse.ocl.examples.pivot/model-gen/Pivot.oclas", true);
+		//
+		//	Load OCLmetamodel as pre-code-generated Java.
+		//
+		Library asLibrary = (Library) metaModelManager.getOclAnyType().getPackage();
+		org.eclipse.ocl.examples.pivot.Package oclMetamodel = OCLMetaModel.create(metaModelManager, asLibrary.getName(), asLibrary.getNsPrefix(), OCLMetaModel.PIVOT_URI);
+		Resource javaResource = oclMetamodel.eResource();
+		doLoadAS(resourceSet, pivotURI, javaResource, false);		// FIXME Contents are far from identical
 	}
 }
