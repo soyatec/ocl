aa3b84f29a08827733ecc5ae172c77ea3f435d50
Ed Willink <ed@willink.me.uk>
2013-11-11 20:46:27 +0000

    [releng] Add promotion of build

--- releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-post-buckminster.sh
+++ releng/org.eclipse.ocl.releng.buckminster/scripts/ocl-post-buckminster.sh
@@ -13,6 +13,8 @@
 mv buildroot/buckminster.output/org.eclipse.ocl.releng.build_*-eclipse.feature/site.p2 MDT-OCL.p2.repository
 mv buildroot/buckminster.output/org.eclipse.ocl.releng.build_*-eclipse.feature/zips MDT-OCL.downloads
 
+/opt/public/common/apache-ant-1.8.1/bin/ant -f publishroot/publisher.ant -Dbuild.archives=${WORKSPACE} 
+
 if [ ${MANAGE_JAVADOC} = "true" ]
 then
   mkdir MDT-OCL.javadoc
