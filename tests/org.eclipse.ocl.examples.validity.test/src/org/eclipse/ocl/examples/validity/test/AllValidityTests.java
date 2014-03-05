fef2c140bf91f2c5b808063e5de0c189cc577732
Ed Willink <ed@willink.me.uk>
2014-03-05 12:16:11 +0000

    [425799] Try just ValidityModelTests (no XPath)

--- tests/org.eclipse.ocl.examples.validity.test/src/org/eclipse/ocl/examples/validity/test/AllValidityTests.java
+++ tests/org.eclipse.ocl.examples.validity.test/src/org/eclipse/ocl/examples/validity/test/AllValidityTests.java
@@ -37,9 +37,9 @@ public class AllValidityTests
 
 	public static void buildSuite(TestSuite suite) {
 		if (EMFPlugin.IS_ECLIPSE_RUNNING) {
-			suite.addTestSuite(HTMLExportOCLValidationResultTests.class);
-			suite.addTestSuite(TextExportOCLValidationResultTests.class);
-			suite.addTestSuite(ValidityManagerTests.class);
+//			suite.addTestSuite(HTMLExportOCLValidationResultTests.class);
+//			suite.addTestSuite(TextExportOCLValidationResultTests.class);
+//			suite.addTestSuite(ValidityManagerTests.class);
 			suite.addTestSuite(ValidityModelTests.class);
 		}
 	}
