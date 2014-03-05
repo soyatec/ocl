1f7c13126c029377303b071e32e02febb3fd5842
Ed Willink <ed@willink.me.uk>
2014-03-05 08:24:23 +0000

    [425799] Rename emfv plugins to examples

--- emfv/org.eclipse.ocl.examples.debug.ui/src/org/eclipse/ocl/examples/internal/debug/ui/launcher/ISetMessage.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.ocl.examples.internal.debug.ui.launcher;
-
-public interface ISetMessage {
-    void setErrorMessage(String message);
-    void setMessage(String message);
-}
--- emfv/org.eclipse.ocl.examples.debug.ui/src/org/eclipse/ocl/examples/internal/debug/ui/launcher/ISetMessageEx.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.ocl.examples.internal.debug.ui.launcher;
-
-/**
- * 
- * @author sboyko
- */
-public interface ISetMessageEx extends ISetMessage {
-    void setWarningMessage(String message);
-}
